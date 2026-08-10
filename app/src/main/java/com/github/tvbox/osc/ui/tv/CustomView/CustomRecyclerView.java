6b1249880c3d8d57bf22a4eae6813bc4180caed7
9602894 <9602894@qq.com>
2026-08-10 08:51:31 +0800

    Create CustomRecyclerView.java

---
+++ app/src/main/java/com/github/tvbox/osc/ui/tv/CustomView/CustomRecyclerView.java
@@ -0,0 +1,19 @@
+package com.github.tvbox.osc.ui.tv.CustomView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CustomRecyclerView extends RecyclerView {
+    public CustomRecyclerView(Context context) {
+        super(context);
+    }
+    public CustomRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public CustomRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
