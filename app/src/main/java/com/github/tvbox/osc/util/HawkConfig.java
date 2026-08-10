3b75abd1c72f9c4c805046752e6277b090e7512b
9602894 <9602894@qq.com>
2026-08-10 08:50:43 +0800

    Update HawkConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -1,10 +1,5 @@
 package com.github.tvbox.osc.util;
 
-/**
- * @author pj567
- * @date :2020/12/23
- * @description:
- */
 public class HawkConfig {
     public static final String API_URL = "api_url";
     public static final String EPG_URL = "epg_url";
@@ -17,20 +12,20 @@ public class HawkConfig {
     public static final String HOME_API = "home_api";
     public static final String DEFAULT_PARSE = "parse_default";
     public static final String DEBUG_OPEN = "debug_open";
-    public static final String PARSE_WEBVIEW = "parse_webview"; // true 系统 false xwalk
+    public static final String PARSE_WEBVIEW = "parse_webview";
     public static final String IJK_CODEC = "ijk_codec";
-    public static final String PLAY_TYPE = "play_type";//0 系统 1 ijk 2 exo 10 MXPlayer
-    public static final String LIVE_PLAY_TYPE = "live_play_type";//0 系统 1 ijk 2 exo 10 MXPlayer
-    public static final String PLAY_RENDER = "play_render"; //0 texture 2
-    public static final String PLAY_SCALE = "play_scale"; //0 texture 2
+    public static final String PLAY_TYPE = "play_type";
+    public static final String LIVE_PLAY_TYPE = "live_play_type";
+    public static final String PLAY_RENDER = "play_render";
+    public static final String PLAY_SCALE = "play_scale";
     public static final String LIVE_PLAY_SCALE = "live_play_scale";
-    public static final String PLAY_TIME_STEP = "play_time_step"; //0 texture 2
+    public static final String PLAY_TIME_STEP = "play_time_step";
     public static final String DOH_URL = "doh_url";
     public static final int DEFAULT_HOME_REC = 1;
-    public static final String HOME_REC = "home_rec"; // 0 豆瓣热播 1 数据源推荐 2 历史
+    public static final String HOME_REC = "home_rec";
     public static final String HISTORY_NUM = "history_num";
     public static final String HISTORY_MERGE = "history_merge";
-    public static final String SEARCH_VIEW = "search_view"; // 0 列表 1 缩略图
+    public static final String SEARCH_VIEW = "search_view";
     public static final String LIVE_CHANNEL = "last_live_channel_name";
     public static final String LIVE_CHANNEL_REVERSE = "live_channel_reverse";
     public static final String LIVE_CROSS_GROUP = "live_cross_group";
@@ -43,7 +38,7 @@ public class HawkConfig {
     public static final String SUBTITLE_TIME_DELAY = "subtitle_time_delay";
     public static final String SOURCES_FOR_SEARCH = "checked_sources_for_search";
     public static final String HOME_REC_STYLE = "home_rec_style";
-    public static final String NOW_DATE = "now_date"; //当前日期
+    public static final String NOW_DATE = "now_date";
     public static final String REMOTE_TVBOX = "remote_tvbox_host";
     public static final String IJK_CACHE_PLAY = "ijk_cache_play";
     public static final String PLAYER_IS_LIVE = "player_is_live";
@@ -64,5 +59,10 @@ public class HawkConfig {
     public static final String DANMU_SIZE_SCALE = "danmu_size_scale";
     public static final String DANMU_RANDOM_COLOR = "danmu_random_color";
     public static final String DANMU_API = "danmu_api";
+    
+    // ========== 酷9兼容常量 ==========
+    public static final String LIVE_PLAYER_TYPE = "live_player_type";
+    // ========== 兼容常量结束 ==========
+    
     public static boolean hotVodDelete;
 }
